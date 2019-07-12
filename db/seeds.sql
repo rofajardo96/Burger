ae86a80add47f2d55327bcb2062b476391dd0989
Robbie F <el.robbie96@gmail.com>
2019-07-11 22:33:30 -0500

    added all of the relevant folders. Need to work on CSS and integrate handlebars

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("The Power Burger", false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("The Mega Burger", false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("The Ultra Burger", false);
+
