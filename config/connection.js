d2cdbcbd781d3f9f18c40f12ab517f57daaa8642
Robbie F <el.robbie96@gmail.com>
2019-07-23 19:01:08 -0500

    changed config file

--- config/connection.js
+++ config/connection.js
@@ -1,34 +1,31 @@
-var mysql = require("mysql");
+var mysql = require ('mysql');
+
 var connection;
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Pablo@12396',
-    database: 'burgers_db'
-  });
+if (process.env.JAWSDB_URL)
+{
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else
+{
+	connection = mysql.createConnection(
+	{
+		host: 'localhost',
+		user: 'root',
+		password: 'Pablo@12396',
+		database: 'burgers_db'
+	});
 };
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Pablo@12396",
-  database: "burgers_db"
-});
 
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
+connection.connect(function(err) 
+{
+  if (err) 
+  {
+    console.error('error connecting: ' + err.stack);
     return;
-  }
-  console.log("connected as id " + connection.threadId);
+  };
+  console.log('connected as id ' + connection.threadId);
 });
 
-// Export connection for our ORM to use.
-connection.connection();
 module.exports = connection;
